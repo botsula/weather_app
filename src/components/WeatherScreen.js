a8dff7fcda4631bd4d6ae41f1cb5389d33805e75
Anna Botsula <anna.botsula@gmail.com>
2021-02-02 23:50:55 +0200

    API weather ver.2

--- src/components/WeatherScreen.js
+++ src/components/WeatherScreen.js
@@ -4,35 +4,37 @@ const WeatherScreen = ({apiRespons}) => {
 
     console.log("TEST: ", apiRespons, "LEN: ", Object.keys(apiRespons).length);
     return(
-      <div className="weather-screen">
-          <h2>{apiRespons.location.name}</h2>
-          <p>{apiRespons.location.country}</p>
-          <div className="weather-set">
-              <p className="temperature">{apiRespons.current.temperature} &#176;C</p>
-              <p>Feels like {apiRespons.current.feelslike} &#176;C</p>
-              <p>Pressure {apiRespons.current.pressure} MB</p>
-              <p>Humidity {apiRespons.current.humidity} %</p>
-              <p>Cloud coverage {apiRespons.current.cloudcover} %</p>
-              <p>Visibility {apiRespons.current.visibility} km</p>
-          </div>
+        <div>
+    {/* //   <div className="weather-screen">
+    //       <h2>{apiRespons.location.name}</h2>
+    //       <p>{apiRespons.location.country}</p>
+    //       <div className="weather-set">
+    //           <p className="temperature">{apiRespons.current.temperature} &#176;C</p>
+    //           <p>Feels like {apiRespons.current.feelslike} &#176;C</p>
+    //           <p>Pressure {apiRespons.current.pressure} MB</p>
+    //           <p>Humidity {apiRespons.current.humidity} %</p>
+    //           <p>Cloud coverage {apiRespons.current.cloudcover} %</p>
+    //           <p>Visibility {apiRespons.current.visibility} km</p>
+    //       </div> */}
         {Object.keys(apiRespons).length === 3 ? (
-
-            <h2>{apiRespons.location.name}</h2>/
-            <p>{apiRespons.location.country}</p>/
-            <div className="weather-set">
-                <p className="temperature">{apiRespons.current.temperature} &#176;C</p>
-                <p>Feels like {apiRespons.current.feelslike} &#176;C</p>
-                <p>Pressure {apiRespons.current.pressure} MB</p>
-                <p>Humidity {apiRespons.current.humidity} %</p>
-                <p>Cloud coverage {apiRespons.current.cloudcover} %</p>
-                <p>Visibility {apiRespons.current.visibility} km</p>
+            <div className="weather-screen">
+                <h2>{apiRespons.location.name}</h2>
+                <p>{apiRespons.location.country}</p>
+                <div className="weather-set">
+                    <p className="temperature">{apiRespons.current.temperature} &#176;C</p>
+                    <p>Feels like {apiRespons.current.feelslike} &#176;C</p>
+                    <p>Pressure {apiRespons.current.pressure} MB</p>
+                    <p>Humidity {apiRespons.current.humidity} %</p>
+                    <p>Cloud coverage {apiRespons.current.cloudcover} %</p>
+                    <p>Visibility {apiRespons.current.visibility} km</p>
+                </div>
             </div>
         
         // <p>Yes, Current temperature in {apiRespons.location.name}</p>
         
         ) : (
         
-        <p>Noooo</p>
+        <p className="no-result">No result 🤔</p>
         
         )}
       </div>
